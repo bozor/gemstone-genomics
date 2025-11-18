4c41bce15949464368046ed733936e9e67af7149
Boris <2324206+bozor@users.noreply.github.com>
2025-11-18 15:07:53 +0000

    add transparent logo and subtle divider for smaller screens in main layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,26 @@
-import clsx from "clsx";
 import { ReactNode } from "react";
+import clsx from "clsx";
+
+import Footer from "./Footer";
 
 import s from "./Main.module.css";
 
 type MainProps = {
   children: ReactNode;
   className: string;
+  showBg?: boolean;
 };
 
-const Main = ({ children, className }: MainProps) => {
-  return <main className={clsx(s.wrap, className)}>{children}</main>;
+const Main = ({ children, className, showBg = true }: MainProps) => {
+  return (
+    <>
+      <main className={clsx(s.wrap, className)}>
+        {children}
+        {showBg ? <span className={s.logoBg}></span> : null}
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default Main;
